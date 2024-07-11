e39c3937cfae20c399701fcf1b992e233f1f24a9
Yeray Blanco de la Fuente <yerayblancodelafuente@MacBook-Air-de-Yeray.local>
2024-07-12 00:20:26 +0200

    Avance C01 Ejercicios extras

---
+++ C01/ex06/ft_strlen.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+void	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i] !='\0')
+	{
+		i++;
+	}
+	return(i);
+}
---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,19 @@
+void	ft_rev_int_tab(int *tab int size)
+{
+	int	i;
+	int	ilen;
+
+	tab = 0;
+	size = 0;
+	i = 0;
+	while(tab[ilen] != '\0')
+	{
+		ilen++;
+	}
+	while(tab[i] != (ilen / 2))
+	{
+		tab[i] = iaux1
+		
+	}
+}
+	
